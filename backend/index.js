95f64ea0d982e7b0ac7ba49cf6f06a75a5eed5fd
SirTwirl <j.kruk123@o2.pl>
2026-09-13 21:53:31 +0200

    feat(backend): add validation for name and number uniqueness

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = 
 [
     { 
@@ -51,6 +53,32 @@ app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number missing' 
+    })
+  }
+
+  const nameExists = persons.some(person => person.name === body.name)
+  if (nameExists) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  const person = {
+    id: String(Math.floor(Math.random() * 1000000)),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 const PORT = 3001
