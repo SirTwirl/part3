a236d7d3ff42d3046d029acd3e9eb6885b1c8c54
SirTwirl <j.kruk123@o2.pl>
2026-09-13 21:27:01 +0200

    feat: add /info endpoint

--- backend/index.js
+++ backend/index.js
@@ -29,5 +29,13 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const currentDate = new Date()
+    response.send(`
+        <h1>Phonebook has info for ${persons.length} people</h1>
+        <p>${currentDate}</p>
+    `)
+})
+
 const PORT = 3001
 app.listen(PORT)
