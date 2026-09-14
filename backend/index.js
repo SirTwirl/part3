e5f7b76e6effffe623895fd221a94bfc631ed82f
SirTwirl <j.kruk123@o2.pl>
2026-09-14 20:11:10 +0200

    fix(backend): use __dirname for static files path

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const path = require('path')
 
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.use(cors())
 
