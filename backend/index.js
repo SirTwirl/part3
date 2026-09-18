5ef1e89b6516fa5802231baf5a5603f4e0944a50
SirTwirl <j.kruk123@o2.pl>
2026-09-18 23:39:39 +0200

    feat: add frontend error handling for mongoose validation errors

--- backend/index.js
+++ backend/index.js
@@ -44,17 +44,12 @@ app.get('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
-
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
+  const { name, number } = request.body
 
   Person.findByIdAndUpdate(
     request.params.id, 
-    person, 
-    { returnDocument: 'after' }
+    { name, number }, 
+    { returnDocument: 'after', runValidators: true, context: 'query' }
   )
     .then(updatedPerson => {
       response.json(updatedPerson)
@@ -100,6 +95,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
